94b3227bc74f63dc1bd5df8a1576636f26b38b63
William Perdue <wlperdue@perdueweb.com>
2022-03-09 21:43:48 -0800

    Update .gitignore Update Dockerfile Add prometheus pushgateway functionality implement argsparser

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM python:3.9.7-slim-buster
+FROM python:3.10-slim-buster
 
 RUN apt-get update \
   && apt-get install git -y
@@ -6,4 +6,4 @@ RUN pip install git+https://github.com/shenxn/libdyson.git@main \
   && pip install influxdb
 
 ADD ./dyson-graph/* .
-CMD ["python", "__init__.py"]
+CMD ["python", "__init__.py", "--influxdb"]
--- dyson-graph/dysonsensors.py
+++ dyson-graph/dysonsensors.py
@@ -10,8 +10,9 @@ def get_dyson_readings(dyson_ip: str, dyson_username: str, dyson_password: str) 
         dyson_username, dyson_password, ld.DEVICE_TYPE_PURE_COOL_LINK
     )
     device.connect(dyson_ip)
-    readings = {}
-    readings["temperature"] = kelvin_to_fahrenheit(device.temperature)
-    readings["humidity"] = device.humidity
+    readings = {
+        "temperature": kelvin_to_fahrenheit(device.temperature),
+        "humidity": device.humidity,
+    }
     device.disconnect()
     return readings
